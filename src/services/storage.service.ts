31dca57f905fdecfe22ee7eeb0795879f96e27d3
Janne Räisänen <janne@jraisanen.com>
2020-05-06 15:31:49 +0300

    Add local storage fallback for server side rendering

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,21 +1,29 @@
-import { Injectable } from '@angular/core';
-import { StorageKey } from '../constants/storage';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { LOCAL_STORAGE, StorageKey } from '../constants/storage';
 
 @Injectable({ providedIn: 'root' })
 export class SfStorageService {
+  private localStorage: any;
+
   get accessToken(): string {
-    return localStorage.getItem(StorageKey.AccessToken) ?? '';
+    return this.localStorage.getItem(StorageKey.AccessToken) ?? '';
   }
 
   set accessToken(accessToken: string) {
-    localStorage.setItem(StorageKey.AccessToken, accessToken);
+    this.localStorage.setItem(StorageKey.AccessToken, accessToken);
   }
 
   get cart(): string {
-    return localStorage.getItem(StorageKey.Cart) ?? '';
+    return this.localStorage.getItem(StorageKey.Cart) ?? '';
   }
 
   set cart(cart: string) {
-    localStorage.setItem(StorageKey.Cart, cart);
+    this.localStorage.setItem(StorageKey.Cart, cart);
+  }
+
+  constructor(
+    @Inject(PLATFORM_ID) readonly platformId: string,
+  ) {
+    this.localStorage = platformId === 'browser' ? localStorage : LOCAL_STORAGE;
   }
 }
